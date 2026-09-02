0f5fd2c4a392ab1b088e78196a3dc0604463b310
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-09-02 23:41:00 +0530

    Time: 3 ms (12.02%), Space: 12.2 MB (89.77%) - LeetHub

--- 3024-type-of-triangle/3024-type-of-triangle.py
+++ 3024-type-of-triangle/3024-type-of-triangle.py
@@ -1,10 +1,11 @@
 class Solution(object):
     def triangleType(self, nums):
-        if (nums[0] + nums[1] <= nums[2]) or (nums[0] + nums[2] <= nums[1]) or (nums[1] + nums[2] <= nums[0]):
+        nums.sort()
+        if nums[0] + nums[1] <= nums[2]:
             return "none"
-        elif nums[0] == nums[1] == nums[2]:
+        elif nums[0] == nums[2]:
             return "equilateral"
-        elif (nums[0] == nums[1]) or (nums[1] == nums[2]) or (nums[0] == nums[2]):
+        elif nums[0] == nums[1] or nums[1] == nums[2]:
             return "isosceles"
         else:
             return "scalene"
