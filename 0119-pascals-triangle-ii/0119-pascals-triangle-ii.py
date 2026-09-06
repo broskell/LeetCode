65ae6a696493158de9f354adff563bdfae7cb9a1
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-09-06 20:54:16 +0530

    Time: 0 ms (100%), Space: 12.2 MB (90.22%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,7 +1,9 @@
-import math
 class Solution(object):
     def getRow(self, rowIndex):
-        pascal = [1] * (rowIndex + 1)
-        for i in range(1, rowIndex, +1):
-            pascal[i] = math.factorial(rowIndex) // (math.factorial(i) * math.factorial(rowIndex - i))
-        return pascal
+        row, prevVal = [1], 1
+
+        for i in range(1, rowIndex + 1, +1):
+            nextVal = prevVal * (rowIndex - i + 1) // i
+            row.append(nextVal)
+            prevVal = nextVal
+        return row
