3cdc746d0d2bafcf0307cf384af5687cbcca8000
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-09-09 14:38:12 +0530

    Time: 0 ms (100%), Space: 12.6 MB (70.83%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -1,5 +1,3 @@
 class Solution(object):
     def getConcatenation(self, nums):
-        for i in range(0, len(nums), +1):
-            nums.append(nums[i])
-        return nums
+        return nums + nums
