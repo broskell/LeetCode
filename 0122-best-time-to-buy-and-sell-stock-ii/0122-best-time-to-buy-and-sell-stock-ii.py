58387347027be88dd3f4b54bb8d5f27211efb2d2
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-09-09 23:38:41 +0530

    Time: 4 ms (40.77%), Space: 13.9 MB (8.32%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        max_profit = 0
+
+        for i in range(1, len(prices), +1):
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]      
+        return max_profit
