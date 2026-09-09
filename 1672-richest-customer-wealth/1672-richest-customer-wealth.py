18d54ca4fa7fcf5926a18cc2ee5f24d3c6ee07e2
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-09-09 15:15:45 +0530

    Time: 0 ms (100%), Space: 12.4 MB (26.72%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def maximumWealth(self, accounts):
-        maxi = sum(accounts[0])
-        for i in range(1, len(accounts) - 1, +1):
-            if sum(accounts[i]) > maxi:
-                maxi = sum(accounts[i])
+        maxi = 0
+        for acc in accounts:
+            maxi = max(maxi, sum(acc))
         return maxi
