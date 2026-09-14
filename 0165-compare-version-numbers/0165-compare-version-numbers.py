22974424cc48b70e33ec470d6006b42ca6cee69a
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-09-14 23:36:22 +0530

    Time: 0 ms (100%), Space: 12.3 MB (56.59%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def compareVersion(self, version1, version2):
+        v1 = version1.split(".")
+        v2 = version2.split(".")
+
+        maxLength = max(len(v1), len(v2))
+
+        for i in range(0, maxLength, +1):
+            if i < len(v1):
+                num1 = int(v1[i])
+            else:
+                num1 = 0
+            
+            if i < len(v2):
+                num2 = int(v2[i])
+            else:
+                num2 = 0
+        
+            if num1 > num2:
+                return 1
+            elif num1 < num2:
+                return -1
+        else:
+            return 0
