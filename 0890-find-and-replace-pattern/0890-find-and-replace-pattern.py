7f9113b491a107c37078adad32741cebc575d43f
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-09-14 19:24:24 +0530

    Time: 4 ms (10.24%), Space: 12.5 MB (0%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.py
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.py
@@ -1,20 +1,12 @@
 class Solution(object):
     def findAndReplacePattern(self, words, pattern):
-		def find(w): 
-			l = []
-			m = defaultdict(int)
-			i = 0
-			for c in w:
-				if c in m:
-					l.append(m[c])
-				else:
-					i+=1
-					m[c]=i
-					l.append(m[c])
-			return l
-		ans = []
-		pfind = find(pattern)
-		for w in words:
-			wfind = find(w)
-			if wfind == pfind: ans.append(w) 
-		return ans
+        def match(word):
+            a = {}
+            b = {}
+            for x, y in zip(word,pattern):
+                if (x in a and a[x] != y) or (y in b and b[y] != x):
+                    return False
+                a[x] = y
+                b[y] = x
+            return True
+        return [word for word in words if match(word)]
