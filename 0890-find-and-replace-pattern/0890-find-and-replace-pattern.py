d6c04daccdae3fd0b01e603ca56aa2f785ba4c52
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-09-14 21:25:02 +0530

    Time: 3 ms (22.83%), Space: 12.3 MB (92.13%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.py
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.py
@@ -1,12 +1,22 @@
 class Solution(object):
     def findAndReplacePattern(self, words, pattern):
-        def match(word):
-            a = {}
-            b = {}
-            for x, y in zip(word,pattern):
-                if (x in a and a[x] != y) or (y in b and b[y] != x):
-                    return False
-                a[x] = y
-                b[y] = x
-            return True
-        return [word for word in words if match(word)]
+        def get_structure_pattern(word): 
+            structure = []
+            char_to_id = {}
+            current_id = 0
+            
+            for char in word:
+                if char not in char_to_id:
+                    current_id += 1
+                    char_to_id[char] = current_id
+                
+                structure.append(char_to_id[char])
+            return structure
+
+        target_pattern = get_structure_pattern(pattern)
+        matched_words = []
+        
+        for word in words:
+            if get_structure_pattern(word) == target_pattern:
+                matched_words.append(word)        
+        return matched_words
