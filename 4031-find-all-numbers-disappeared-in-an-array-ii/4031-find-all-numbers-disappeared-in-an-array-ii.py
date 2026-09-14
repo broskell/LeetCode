a52b79408711dedaf2d3e981a307e132569d9d1e
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-09-14 23:03:35 +0530

    Time: 267 ms (43.62%), Space: 32.4 MB (32.31%) - LeetHub

--- 4031-find-all-numbers-disappeared-in-an-array-ii/4031-find-all-numbers-disappeared-in-an-array-ii.py
+++ 4031-find-all-numbers-disappeared-in-an-array-ii/4031-find-all-numbers-disappeared-in-an-array-ii.py
@@ -1,13 +1,15 @@
 class Solution(object):
     def findDisappearedNumbers(self, nums, lower, upper):
-        cleanedNums = sorted(list(set(num for num in nums if lower <= num <= upper)))
-        missing, expected = [], lower
+        numsSet = set(nums)
+        missing, i = [], lower
 
-        for num in cleanedNums:
-            if num > expected:
-                missing.append([expected, num - 1])
-            expected = num + 1
-            
-        if expected <= upper:
-            missing.append([expected, upper])
+        while i <= upper:
+            if i not in numsSet:
+                expected = i
+
+                while i <= upper and i not in numsSet:
+                    i += 1
+                missing.append([expected, i - 1])
+            else:
+                i += 1
         return missing
