23eedf33179ddb8ff393fb8bb7c27c6d3ed2718e
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-09-19 23:35:13 +0530

    Time: 0 ms (100%), Space: 12.3 MB (92.02%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -2,7 +2,7 @@ class Solution(object):
     def largestAltitude(self, gain):
         high, netGain = 0, 0
 
-        for i, altitude in enumerate(gain):
+        for altitude in gain:
             netGain += altitude
             high = max(high, netGain)
         return high
