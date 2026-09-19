964cef043d8f5696f1c80c662a372934c5c0d906
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-09-19 22:58:52 +0530

    Time: 29 ms (87.86%), Space: 14.9 MB (55%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,13 +1,15 @@
 class Solution(object):
     def subarraysDivByK(self, nums, k):
         curSum, count = 0, 0
-        freq = [0] * k
-        freq[0] = 1
+        freqMap = {0 : 1}
 
         for num in nums:
             curSum += num
             rem = curSum % k
-            count += freq[rem]
 
-            freq[rem] += 1
+            if rem in freqMap:
+                count += freqMap[rem]
+                freqMap[rem] += 1
+            else:
+                freqMap[rem] = 1
         return count
