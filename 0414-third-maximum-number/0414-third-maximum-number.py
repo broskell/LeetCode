5baed0868dce104b213d654fefb67fa0f5718d46
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-09-22 22:25:47 +0530

    Time: 0 ms (100%), Space: 12.8 MB (82.13%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def thirdMax(self, nums):
+        max1, max2, max3 = float('-inf'), float('-inf'), float('-inf')
+
+        for num in nums:
+            if num == max1 or num == max2 or num == max3:
+                continue
+                
+            if num > max1:
+                max3 = max2
+                max2 = max1
+                max1 = num
+            elif num > max2:
+                max3 = max2
+                max2 = num
+            elif num > max3:
+                max3 = num
+
+        return max3 if max3 != float('-inf') else max1
