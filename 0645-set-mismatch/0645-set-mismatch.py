85ebca77717d277dd05f5690832fc8ecd6160a00
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-09-24 00:35:13 +0530

    Time: 28 ms (34.9%), Space: 13.3 MB (88.39%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,29 +1,24 @@
 class Solution(object):
     def findErrorNums(self, nums):
-        n = len(nums)
         xor_all = 0
         
-        for num in nums:
-            xor_all ^= num
-        for i in range(1, n + 1):
-            xor_all ^= i
+        for i in range(0, len(nums), +1):
+            xor_all ^= nums[i]
+            xor_all ^= i + 1
             
         rightmost_set_bit = xor_all & ~(xor_all - 1)
-        
-        group0 = 0
-        group1 = 0
-        
-        for num in nums:
-            if num & rightmost_set_bit:
-                group1 ^= num
-            else:
-                group0 ^= num
+        group0, group1 = 0, 0
 
-        for i in range(1, n + 1):
-            if i & rightmost_set_bit:
-                group1 ^= i
+        for i in range(0, len(nums), +1):
+            if nums[i] & rightmost_set_bit:
+                group1 ^= nums[i]
             else:
-                group0 ^= i
+                group0 ^= nums[i]
+
+            if i + 1 & rightmost_set_bit:
+                group1 ^= i + 1
+            else:
+                group0 ^= i + 1
                 
         for num in nums:
             if num == group1:
