d0bdb1d872d1d9185d8f77b140d665a551092e80
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-09-26 20:20:11 +0530

    Time: 32 ms (59.04%), Space: 13.9 MB (50.28%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numSubarrayProductLessThanK(self, nums, k):
+        if k <= 1:
+            return 0
+
+        count, pdt, left = 0, 1, 0
+
+        for right in range(0, len(nums), +1):
+            pdt *= nums[right]
+
+            while pdt >= k:
+                pdt /= nums[left]
+                left += 1
+            
+            count += (right - left + 1)
+        return count
